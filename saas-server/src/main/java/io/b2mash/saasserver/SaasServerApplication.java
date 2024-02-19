7dd6583fdc295c98363a70343c2fb732f33fc6c8
Rakheen Dama <rakheen.dama@binarymash.io>
2024-02-19 23:50:56 +0200

    platform compose

---
+++ saas-server/src/main/java/io/b2mash/saasserver/SaasServerApplication.java
@@ -0,0 +1,13 @@
+package io.b2mash.saasserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasServerApplication.class, args);
+	}
+
+}
---
+++ saas-server/src/test/java/io/b2mash/saasserver/SaasServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.b2mash.saasserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
