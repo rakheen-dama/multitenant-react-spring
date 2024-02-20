e34c8da8979f2a519d20a136f56b5803fb155590
Rakheen Dama <rakheen.dama@binarymash.io>
2024-02-20 02:20:16 +0200

    platform configurations wip

---
+++ platform/docker-compose/postgres/init.sql
@@ -0,0 +1,3 @@
+create schema if not exists keycloak;
+create schema if not exists b2bserver;
+create schema if not exists tenant;
--- platform/postgres/init.sql
+++
@@ -1,2 +0,0 @@
-create schema if not exists keycloak;
-create schema if not exists intros;
