8a35bc853c4d458b87e3155cc66076216790e7d6
Rakheen Dama <rakheen.dama@binarymash.io>
2024-03-10 15:04:14 +0200

    add subscriber-service

---
+++ subscriber-service/src/main/java/io/b2mash/subscriber/SubscriberServiceApplication.java
@@ -0,0 +1,13 @@
+package io.b2mash.subscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriberServiceApplication.class, args);
+    }
+
+}
---
+++ subscriber-service/src/test/java/io/b2mash/subscriber/SubscriberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.b2mash.subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
